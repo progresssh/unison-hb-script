5b87697e3d52d9d71af081ce367bb07974ce46d4
progress <star@progress.sh>
2023-04-27 05:18:55 +0200

    added anchor tag, changed const to let for cargo

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const jobDiv = document.getElementById("unison-job-openings-children");
-console.log(jobDiv);
+let jobDiv = document.getElementById("unison-job-openings-children");
+
 async function fetchJobOpenings() {
   const res = await fetch(
     "https://api.ashbyhq.com/posting-api/job-board/UNISON"
   );
   const data = await res.json();
-  console.log(data);
+
   data.jobs.map((job) => {
-    let element = document.createElement("p");
+    let element = document.createElement("a");
+    element.href = job.jobUrl;
+    element.target = "_blank";
     element.innerText = `${job.title}\nSan Francisco, CA / Mountain View, CA`;
     jobDiv.appendChild(element);
-    console.log("after" + jobDiv);
   });
 }
 
