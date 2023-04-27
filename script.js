564a3c341080180be5f8388b7e46e7484c094506
progress <star@progress.sh>
2023-04-27 05:37:35 +0200

    added placeholder handling

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const jobDiv = document.getElementById("unison-job-openings");
+const placeholder = document.getElementById("unison-placeholder");
 
 async function fetchJobOpenings() {
+  if (!jobDiv) {
+    console.error(
+      'Please add an element with this specific id: "unison-job-openings"'
+    );
+  }
+
   const res = await fetch(
     "https://api.ashbyhq.com/posting-api/job-board/UNISON"
   );
@@ -15,6 +22,10 @@ async function fetchJobOpenings() {
     link.target = "_blank";
     link.innerText = "→ Read More";
     jobParagraph.innerText = `${job.title}\nSan Francisco, CA / Mountain View, CA\n`;
+
+    if (placeholder) {
+      placeholder.remove();
+    }
 
     element.appendChild(jobParagraph);
     element.appendChild(link);
